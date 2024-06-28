1af4a1651ca4de6743539c3ba7e3b1880c7ff573
Sinan Akdal <snnakdal@gmail.com>
2024-06-28 17:33:14 +0300

    commit

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ app.use(cors());
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
+let PORT = process.env.PORT || 5000;
+
 app.get("/", (req, res) => {
   res.json({ message: "denemeee" });
 });
@@ -30,7 +32,7 @@ app.use("/", UserCreditCart);
 app.use("/", FeedBack);
 
 db();
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
   console.log("SERVER IS RUNNING ON PORT 5000");
 });
 
